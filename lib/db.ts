8c41964aed385a6cfa2855f0de12dc9fac364a59
souravroy-dev <devils_wolf@protonmail.com>
2026-06-28 22:03:28 +0600

    Switch to @prisma/adapter-neon for faster serverless cold starts (fixes login timeout)

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,10 @@
 import { PrismaClient } from '@/prisma/client/client'
-import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaNeon } from '@prisma/adapter-neon'
 
 const g = globalThis as unknown as { prisma: PrismaClient }
 
 function createClient() {
-  const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
+  const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL! })
   return new PrismaClient({ adapter })
 }
 
